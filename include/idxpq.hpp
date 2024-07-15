f2d3455f195ab7dd73dcb5c4ebceade80e8cc452
Antônio <142094210+santosantonio1@users.noreply.github.com>
2024-07-14 21:06:34 -0300

    fix idxpq identation

--- include/idxpq.hpp
+++ include/idxpq.hpp
@@ -20,125 +20,125 @@ template<
 > class IndexPQ
     {
         public:
-        IndexPQ(const size_t nmax)
-        {
-            N = 0;
-            keys.resize(nmax+1);
-            heap.resize(nmax+1);
-            idx.resize(nmax+1);
-
-            std::fill(idx.begin(), idx.end(), 0);            
-        }
-        
-        void push(const size_t k, const Key& key)
-        {
-            if(contains(k)) return;
-
-            idx[k] = ++N;
-            heap[N] = k;
-            keys[k] = key;
-            swim(N);
-        }
-
-        size_t pop()
-        {
-            if(N==0) return 0;
-            const size_t t = heap[1];
-            swap(1, N--);
-            sink(1);
-
-            idx[t] = 0;
-
-            return t;
-        }
-
-        void change(const size_t k, const Key& key)
-        {
-            if(!contains(k)) return;
-
-            keys[k] = key;
-            swim(idx[k]);
-            sink(idx[k]);
-        }
-
-        void increaseKey(const size_t i, const Key& key)
-        {
-            if(
-                !contains(i)    ||
-                cmp(keys[i], key)
-            ) return;
-
-            keys[i] = key;
-            sink(idx[i]);
-        }
-
-        void decreaseKey(const size_t i, const Key& key)
-        {
-            if(
-                !contains(i)    ||
-                !cmp(keys[i], key)
-            ) return;
-
-            keys[i] = key;
-            swim(idx[i]);
-        }
-
-        bool contains(const size_t k) const {
-            return idx[k] != 0;
-        }
-
-        bool empty() const {
-            return N == 0;
-        }
-
-        size_t size() const {
-            return N;
-        }
-
-        size_t top() const {
-            return heap[1];
-        }
-
-        Key topKey() const {
-            return keys[heap[1]];
-        }
-
-        auto begin() const {
-            return ++heap.begin();
-        }
-
-        auto cbegin() const {
-            return ++heap.cbegin();
-        }
-
-        auto rbegin() const {
-            return heap.rbegin();
-        }
-
-        auto end() const {
-            return heap.end();
-        }
-
-        auto cend() const{
-            return heap.cend();
-        }
-
-        auto rend() const{
-            return ++heap.cend();
-        }
-
-        std::string str_k() const {
-            std::stringstream ss;
-            for(const Key& k : keys)
-                ss << k << " ";
-
-            ss << '\n';
-            return ss.str();
-        }
-
-        size_t operations() const {
-            return op;
-        }
+            IndexPQ(const size_t nmax)
+            {
+                N = 0;
+                keys.resize(nmax+1);
+                heap.resize(nmax+1);
+                idx.resize(nmax+1);
+    
+                std::fill(idx.begin(), idx.end(), 0);            
+            }
+            
+            void push(const size_t k, const Key& key)
+            {
+                if(contains(k)) return;
+    
+                idx[k] = ++N;
+                heap[N] = k;
+                keys[k] = key;
+                swim(N);
+            }
+    
+            size_t pop()
+            {
+                if(N==0) return 0;
+                const size_t t = heap[1];
+                swap(1, N--);
+                sink(1);
+    
+                idx[t] = 0;
+    
+                return t;
+            }
+    
+            void change(const size_t k, const Key& key)
+            {
+                if(!contains(k)) return;
+    
+                keys[k] = key;
+                swim(idx[k]);
+                sink(idx[k]);
+            }
+    
+            void increaseKey(const size_t i, const Key& key)
+            {
+                if(
+                    !contains(i)    ||
+                    cmp(keys[i], key)
+                ) return;
+    
+                keys[i] = key;
+                sink(idx[i]);
+            }
+    
+            void decreaseKey(const size_t i, const Key& key)
+            {
+                if(
+                    !contains(i)    ||
+                    !cmp(keys[i], key)
+                ) return;
+    
+                keys[i] = key;
+                swim(idx[i]);
+            }
+    
+            bool contains(const size_t k) const {
+                return idx[k] != 0;
+            }
+    
+            bool empty() const {
+                return N == 0;
+            }
+    
+            size_t size() const {
+                return N;
+            }
+    
+            size_t top() const {
+                return heap[1];
+            }
+    
+            Key topKey() const {
+                return keys[heap[1]];
+            }
+    
+            auto begin() const {
+                return ++heap.begin();
+            }
+    
+            auto cbegin() const {
+                return ++heap.cbegin();
+            }
+    
+            auto rbegin() const {
+                return heap.rbegin();
+            }
+    
+            auto end() const {
+                return heap.end();
+            }
+    
+            auto cend() const{
+                return heap.cend();
+            }
+    
+            auto rend() const{
+                return ++heap.cend();
+            }
+    
+            std::string str_k() const {
+                std::stringstream ss;
+                for(const Key& k : keys)
+                    ss << k << " ";
+    
+                ss << '\n';
+                return ss.str();
+            }
+    
+            size_t operations() const {
+                return op;
+            }
 
         private:
             size_t N;
